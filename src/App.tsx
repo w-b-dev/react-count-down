96ea7e1dfb62210e53e498442fab878d06bad924
Daniel Santos <daniel@codein.ca>
2020-04-10 00:22:08 -0400

    simple countdown for days implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import Display from "./components/Display";
-import SetDate from "./components/SetDate";
+import Display from './components/Display';
+import SetDate from './components/SetDate';
 
 function App() {
-
-    const targetDate = "Jan 17, 2021 12:34:56";
+  const targetDate = 'Jan 17, 2021 12:34:56';
 
   return (
     <div className="App">
       <Display targetDate={targetDate} />
-      <SetDate targetDate={targetDate}/>
+      <SetDate targetDate={targetDate} />
     </div>
   );
 }
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Display.css';
 
 interface DisplayProps {
-    targetDate: string
+  targetDate: string;
 }
 
-const Display = ({targetDate}: DisplayProps) => (
+const Display = ({ targetDate }: DisplayProps) => {
+  const countDownDate = new Date(targetDate).getTime();
+  const oneDayInMs = 1000 * 60 * 60 * 24;
+  const now = new Date().getTime();
+  let timeGap = Math.floor((countDownDate - now) / oneDayInMs);
+
+  return (
     <section className="display" title="countdown display">
-        <span>{targetDate}</span>
+      <span>{timeGap.toString()} days to go</span>
     </section>
-);
+  );
+};
 
 export default Display;
