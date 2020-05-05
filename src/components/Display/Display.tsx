4a0aad4ce59c816305be87c5f27e322e5a5aed50
Daniel Brasileiro <iam@w-b.dev>
2020-05-04 22:47:53 -0400

    styling improvements fix #83 (#84)

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,61 +1,62 @@
 import React from 'react';
 import './Display.css';
-import {CardProps, CardType} from '../interfaces';
+import { CardProps, CardType } from '../interfaces';
 
 export const calculateTimeGap = (state: CardType) => {
   const countDownDate = new Date(state.dateString).getTime();
   const oneDayInMs = 1000 * 60 * 60 * 24;
   const now = new Date().getTime();
-  return  Math.floor((countDownDate - now) / oneDayInMs);
-}
+  return Math.floor((countDownDate - now) / oneDayInMs);
+};
 
 const Display = ({ cardState }: CardProps) => {
   const timeGap = calculateTimeGap(cardState);
 
   const decideMessage = ({
-                           amount,
-                           scale,
-                         }: {
+    amount,
+    scale,
+  }: {
     amount: number;
     scale: string;
   }) => {
     return timeGap > 0
-        ? [amount, scale, 'to go until']
-        : [amount, scale, 'have passed since'];
+      ? [amount, scale, 'to go until']
+      : [amount, scale, 'have passed since'];
   };
 
   const Time = () => {
     const [amount, scale, phrase] = decideMessage(conditionalValue());
-    return <div className="countdown-message">
-      {timeGap === 0
-          ? <span className="today">Today is the day!!!</span>
-          :
+    return (
+      <div className="countdown-message">
+        {timeGap === 0 ? (
+          <span className="today">Today is the day!!!</span>
+        ) : (
           <>
-            <span className="time-amount">
-              {amount}
-            </span>
-            <span className="time-scale">
-              {scale}
-            </span>
-            <span className="time-complement">
-              {phrase}
-            </span>
+            <span className="time-amount">{amount}</span>
+            <div className="wrapper">
+              <span className="time-scale">{scale}</span>
+              <span className="time-complement">{phrase}</span>
+            </div>
           </>
-      }
-    </div>
+        )}
+      </div>
+    );
   };
 
   const conditionalValue = () => {
     const timeGapAbsolute = Math.abs(timeGap);
-    if (timeGapAbsolute < 14) return {amount: timeGapAbsolute, scale: 'days'};
+    if (timeGapAbsolute < 14) return { amount: timeGapAbsolute, scale: 'days' };
     if (timeGapAbsolute < 4 * 30)
-      return {amount: Math.round(timeGapAbsolute / 7), scale: 'weeks'};
+      return { amount: Math.round(timeGapAbsolute / 7), scale: 'weeks' };
     if (timeGapAbsolute < 24 * 30)
-      return {amount: Math.round(timeGapAbsolute / 30), scale: 'months'};
+      return { amount: Math.round(timeGapAbsolute / 30), scale: 'months' };
     // defaults to years
     // TODO: CHECK ISSUE WITH CUMULATIVE IMPRECISION DUE TO ROUNDING AND PREMISES
     // Example: Should not be considering all months to be 30 days and all years 365.
-    return {amount: Math.trunc((timeGapAbsolute / 365) * 10)/10, scale: 'years'};
+    return {
+      amount: Math.trunc((timeGapAbsolute / 365) * 10) / 10,
+      scale: 'years',
+    };
   };
   return (
     <section
@@ -64,8 +65,18 @@ const Display = ({ cardState }: CardProps) => {
       className="Display"
       title="countdown display"
     >
-      <Time/>
-      <span className="title">{cardState.title}</span>
+      <Time />
+      <div className="card-description">
+        <span className="icon"> 💬 </span>
+        <span className="title">{cardState.title}</span>
+        <span className="icon"> 📅 </span>
+        <span id={cardState.id} className="card-date">
+          {cardState.dateString
+            .split(' ')
+            .slice(0, cardState.dateString.split(' ').length - 1)
+            .join(' ')}
+        </span>
+      </div>
     </section>
   );
 };
